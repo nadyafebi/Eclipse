05429642da87c6679dd9e58b4e918148da42828b
mmarkovi <47645338+mmarkovi@users.noreply.github.com>
2019-11-22 02:21:08 -0800

    Finished Layout of 3.2 and 3.3
    Just need to combine the transition between them (didn't want to do it in case of errors between us) and need to make some aesthetic touch ups.

---
+++ Platformer/Assets/Scripts/ButtonAndButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAndButton : MonoBehaviour
+{
+    //public GameObject button;
+    public GameObject block;
+    public GameObject button2;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetComponent<Rigidbody2D>().IsTouching(block.GetComponent<Collider2D>()))
+        {
+            button2.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+}
