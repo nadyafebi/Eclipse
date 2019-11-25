8342d62a376307448c9c8f22f87ed0b6be4e7a6b
mmarkovi <47645338+mmarkovi@users.noreply.github.com>
2019-11-24 22:54:49 -0800

    made Ability to Break Walls
    use left ctrl to break walls when next to. Example blocks are in newly created Stage2Test

---
+++ Platformer/Assets/Scripts/BreakWall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakWall : MonoBehaviour
+{
+    public GameObject player;
+
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && GetComponent<Rigidbody2D>().IsTouching(player.GetComponent<Collider2D>()))
+        {
+            gameObject.SetActive(false);
+
+        }
+
+    }
+}
