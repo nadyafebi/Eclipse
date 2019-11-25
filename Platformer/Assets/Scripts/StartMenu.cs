67c1d65d55f0994cabd44178b364b180e256b5be
Nadya Djojosantoso <hello@nadyafebi.com>
2019-11-24 23:51:56 -0800

    Edit start menu and add animation

--- Platformer/Assets/Scripts/StartMenu.cs
+++ Platformer/Assets/Scripts/StartMenu.cs
@@ -9,17 +9,52 @@ public class StartMenu : MonoBehaviour
     public Button playButton;
     public Button creditsButton;
     public Button quitButton;
+
+    public Image fadeOut;
+
+    public AudioClip menuSound;
+
+    private AudioSource audioSource;
     
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         playButton.onClick.AddListener(play);
         creditsButton.onClick.AddListener(showCredits);
         quitButton.onClick.AddListener(quit);
     }
 
+    void Update()
+    {
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            audioSource.PlayOneShot(menuSound);
+        }
+    }
+
+    /// <summary>
+    /// Dummy function to start the Play co-routine.
+    /// </summary>
     void play()
     {
-        // TODO: Insert some cool transition here!
+        StartCoroutine(play(3.0f));
+    }
+
+    IEnumerator play(float fadeOutDuration)
+    {
+        // Fade the screen to black.
+        fadeOut.color = Color.black;
+        fadeOut.canvasRenderer.SetAlpha(0.0f);
+        fadeOut.CrossFadeAlpha(1.0f, fadeOutDuration, false);
+
+        // Fade the audio too.
+        float startVolume = audioSource.volume;
+        while (audioSource.volume > 0)
+        {
+            audioSource.volume -= startVolume * Time.deltaTime / fadeOutDuration;
+            yield return null;
+        }
+        
         SceneManager.LoadScene("Stage 1.1 Spawn");
     }
 
