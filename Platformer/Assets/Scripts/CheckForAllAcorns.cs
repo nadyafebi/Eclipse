54a2c2613e1de1709a2cbb29725146577742eac8
mmarkovi <47645338+mmarkovi@users.noreply.github.com>
2019-11-29 13:00:59 -0800

    Added Acorn Pickup and Spikes
    Check AcornTest in Stage 1 to see both

---
+++ Platformer/Assets/Scripts/CheckForAllAcorns.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckForAllAcorns : MonoBehaviour
+{
+    public GameObject parentAcorn;
+    public GameObject Tree;
+    //private int initialChildCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //initialChildCount = parentAcorn.transform.childCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int activeChildren = parentAcorn.transform.childCount;
+        if (activeChildren == 0)
+        {
+            Tree.SetActive(true);
+        }
+    }
+}
