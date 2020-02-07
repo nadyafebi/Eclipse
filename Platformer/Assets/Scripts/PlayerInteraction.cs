de86b0cc939217ed7b2569a94349a9df336c7a95
Nadya Djojosantoso <hello@nadyafebi.com>
2020-02-06 23:18:02 -0800

    Add lever system and toggleable block

---
+++ Platformer/Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public bool isTouchingInteractable = false;
+    private GameObject interactable;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && isTouchingInteractable)
+        {
+            interactable.GetComponent<Interactable>().Interact();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Interactable")
+        {
+            isTouchingInteractable = true;
+            interactable = collider.gameObject;
+        }
+    }
+
+    void OnTriggerExit2D()
+    {
+        isTouchingInteractable = false;
+    }
+}
