87c58c3419fe6919c758812b94cfca41509f2b45
hussler <56606099+hussler@users.noreply.github.com>
2020-02-26 01:51:13 -0800

    Double Jump
    Luna starts off with one jump but can double jump when she reaches the first high acorn

---
+++ Platformer/Assets/Scripts/DoubleJumpScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleJumpScene : MonoBehaviour
+{
+    public GameObject player;
+
+    private CharacterController2D controller;
+    private PlayerMovement playerMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = player.GetComponent<CharacterController2D>();
+	playerMovement = player.GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	if (GetComponent<BoxCollider2D>().IsTouching(player.GetComponent<Collider2D>()))
+	{
+	    // ADD DIALOGUE SAYING PLAYER CAN NOW DOUBLE JUMP
+	    controller.m_AirJumps = 1;
+	    Destroy(gameObject); // make luna pause, show text, change to double jump
+	}
+        
+    }
+}
