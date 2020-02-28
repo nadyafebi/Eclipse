4beb830b601d38dbe49998d324c7b4007f6a3d1e
Nadya Djojosantoso <hello@nadyafebi.com>
2020-02-28 00:41:11 -0800

    Add Sol wake up animation

---
+++ Platformer/Assets/Scripts/Cutscenes/WakesUp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[AddComponentMenu("Cutscenes/Wakes Up")]
+public class WakesUp : MonoBehaviour
+{
+    [FormerlySerializedAs("luna")]
+    public GameObject player;
+    public AnimationClip wakeUpAnimation;
+
+    private Animator animator;
+    private PlayerMovement movement;
+
+    void Start()
+    {
+        animator = player.GetComponent<Animator>();
+        movement = player.GetComponent<PlayerMovement>();
+        
+        StartCoroutine(WakeUp());
+    }
+
+    private IEnumerator WakeUp()
+    {
+        movement.movable = false;
+        animator.SetBool("Sleep", true);
+        yield return new WaitForSeconds(wakeUpAnimation.length);
+        animator.SetBool("Sleep", false);
+        movement.movable = true;
+    }
+}
