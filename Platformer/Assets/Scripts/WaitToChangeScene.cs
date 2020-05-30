1290450a2fd5a435106d6dec29feb4213b80aee4
Kari Tu Tran <karitt@uci.edu>
2020-05-29 22:40:22 -0700

    Added Introduction

---
+++ Platformer/Assets/Scripts/WaitToChangeScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WaitToChangeScene : MonoBehaviour
+{
+    public float waitTime;
+    public string nextScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ChangeAfterSeconds(waitTime));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    IEnumerator ChangeAfterSeconds(float seconds)
+    {
+	yield return new WaitForSeconds(seconds);
+	SceneManager.LoadScene(nextScene, LoadSceneMode.Single);
+    }
+
+}
