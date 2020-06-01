7069015a614664ccdd6ed0f8ecfb7c952c62d7a1
Nadya Djojosantoso <hello@nadyafebi.com>
2020-05-31 18:35:50 -0700

    Fix video problem

--- Platformer/Assets/Scripts/WaitToChangeScene.cs
+++ Platformer/Assets/Scripts/WaitToChangeScene.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class WaitToChangeScene : MonoBehaviour
 {
+    public string videoName;
     public float waitTime;
     public string nextScene;
+    private VideoPlayer videoPlayer;
 
     // Start is called before the first frame update
     void Start()
     {
+        videoPlayer = GetComponent<VideoPlayer>();
+        videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, videoName);
+        videoPlayer.Play();
         StartCoroutine(ChangeAfterSeconds(waitTime));
     }
 
